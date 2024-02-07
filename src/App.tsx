3814b25d455022c98a63ba7fd67c48ee8f029a43
Eduardo Farias Camargo <edu.farias.camargo@gmail.com>
2024-02-06 21:00:44 -0300

    Tailwind added in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="mx-auto max-w-6xl my-12">
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
+        />
+      </form>
+    </div>
+  );
 }
