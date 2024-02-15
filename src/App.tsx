483bb58b22a680c322e2b56aef4ad8ed7bb5ab3c
Eduardo Farias Camargo <edu.farias.camargo@gmail.com>
2024-02-15 18:50:11 -0300

    language fixed

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,7 @@ export default function App() {
       <form className="w-full">
         <input
           type="text"
-          placeholder="Busque em suas notas..."
+          placeholder="Search in your notes..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
           onChange={handleSearch}
         />
--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -49,9 +49,9 @@ export default function NoteCard({ note, onNoteDeleted }: NoteCardProps) {
             onClick={() => onNoteDeleted(note.id)}
             className="w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"
           >
-            Deseja{" "}
+            Do you want to{" "}
             <span className="text-red-400 group-hover:underline">
-              apagar essa nota?
+              delete this note?
             </span>
           </button>
         </Dialog.Content>
