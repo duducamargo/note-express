d64df791c8896b9730b39fc1917bb84bac4a4e0c
Eduardo Farias Camargo <edu.farias.camargo@gmail.com>
2024-02-18 20:18:47 -0300

    logo added

--- src/layouts/NavBar.tsx
+++ src/layouts/NavBar.tsx
@@ -1,9 +1,19 @@
 import { Link } from "react-router-dom";
+import logo from "../../public/favicon.png";
 
 function NavBar() {
   return (
     <nav>
-      <ul className="bg-slate-800/20 flex justify-end py-4 px-4 text-slate-200  ">
+      <li className="list-none">
+        <Link to="/">
+          <img
+            className="absolute w-[85px] p-1 ml-6 md:ml-24 mt-[-13px] hover:opacity-70 hover:duration-[0.2s]"
+            src={logo}
+            alt="logo"
+          />
+        </Link>
+      </li>
+      <ul className="bg-slate-800/10 flex justify-end py-4 px-4 text-slate-200  ">
         <li className="mx-4 md:mr-8  hover:text-slate-400 ">
           <Link to="/">Home</Link>
         </li>
