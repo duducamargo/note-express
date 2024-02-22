a3cef52ef765a1bc6c8aef1331ac87357cb7eab5
Eduardo Farias Camargo <edu.farias.camargo@gmail.com>
2024-02-22 20:48:01 -0300

    server added

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
-import { FaLinkedin, FaInstagram, FaGithub, FaLink } from "react-icons/fa";
+import { FaLinkedin, FaInstagram, FaGithub } from "react-icons/fa";
 
 function AboutUsPage() {
   return (
     <div className="text-slate-200 w-5/6 mx-auto">
-      <h1 className="text-center text-3xl mb-4">About Us</h1>
+      <h1 className="text-center text-3xl my-4">About Us</h1>
       <h2 className="mb-2 text-2xl text-slate-200/70">Our Mission</h2>
       <p className="mb-2">
         Our mission is to provide a user-friendly platform that empowers
@@ -42,7 +42,7 @@ function AboutUsPage() {
         Reach out to us at{" "}
         <span className="text-slate-200/70">edu.farias.camargo@gmail.com</span>
       </p>
-      <h2 className="mt-10 text-xl text-center">
+      <h2 className="mt-10 mb-20 md:mb-0 text-xl text-center">
         Thank you for choosing{" "}
         <span className="text-slate-200/70">NoteExpress</span> for your
         note-taking needs. Happy note-taking!
