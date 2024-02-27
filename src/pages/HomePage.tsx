e453ef0e48a3cfddf56ebad0ef8f6be09965c635
Eduardo Farias Camargo <edu.farias.camargo@gmail.com>
2024-02-27 19:46:31 -0300

    responsiveness was fixed

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,14 +3,16 @@ import paperplane from "../../public/paperplane.png";
 
 function HomePage() {
   return (
-    <div className="flex justify-center  text-slate-200 cursor-default ">
-      <div className=" flex flex-col mx-auto bg-sky bg-no-repeat bg-center bg-cover mt-8 p-1 border-[1px] border-white/20 rounded-[10px] md:px-4">
+    <div className="relative flex justify-center  text-slate-200 cursor-default min-w-[362px]">
+      <div className=" flex flex-col mx-auto bg-sky bg-no-repeat bg-center bg-cover mt-8 p-1 border-[1px] border-white/20 rounded-[10px] md:px-4 mb-20 mmd:w-5/6 mmd:max-w-[600px] w-5/6 ">
         <img
           src={paperplane}
-          className="absolute w-40 mt-80 ml-[400px] md:ml-[550px] md:mt-72"
+          className="absolute w-0 mmd:w-40 mmd:ml-[400px] mmd:mt-[360px] md:w-40   md:ml-[550px] md:mt-72"
         ></img>
-        <h1 className="text-6xl mb-9 mt-16 mx-auto">The Best Notes Site.</h1>
-        <p className=" text-slate-100/70 text-xl mb-9 justify-center w-[600px] md:w-[800px] text-center">
+        <h1 className="text-3xl mb-9 mt-16 mx-auto md:text-6xl mmd:text-5xl">
+          The Best Notes Site.
+        </h1>
+        <p className=" text-slate-100/70 tetxt-lg md:text-xl mb-9 justify-center w-[300px] md:w-[800px] text-center xsm:text-slate-900 mx-auto mmd:text-xl mmd:w-[450px]">
           Unlock the power of organization and productivity with our
           cutting-edge note-taking platform. Designed with simplicity and
           efficiency in mind, our site offers the most seamless experience for
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      mmd: "610px",
+    },
     extend: {
       fontFamily: {
         sans: ["Inter", "sans-serif"],
