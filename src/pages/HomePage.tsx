c7e3ec679a0228059f3426e7b09c06630ee8c44b
Eduardo Farias Camargo <edu.farias.camargo@gmail.com>
2024-03-10 23:39:05 -0300

    home interface responsiveness

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,20 +3,22 @@ import paperplane from "../../public/paperplane.png";
 
 function HomePage() {
   return (
-    <div className="flex justify-center  text-slate-200 cursor-default ">
-      <div className=" flex flex-col mx-auto bg-sky bg-no-repeat bg-center bg-cover mt-8 p-1 border-[1px] border-white/20 rounded-[10px] md:px-4">
-        <img
-          src={paperplane}
-          className="absolute w-40 mt-80 ml-[400px] md:ml-[550px] md:mt-72 sm:w-0"
-        ></img>
-        <h1 className="text-6xl mb-9 mt-16 mx-auto">The Best Notes Site.</h1>
-        <p className=" text-slate-100/70 text-xl mb-9 justify-center w-[600px] md:w-[800px] text-center ">
+    <div className="flex justify-center  text-slate-200 cursor-default mb-20">
+      <div className="w-4/5 lg:w-3/5 flex flex-col mx-auto bg-sky bg-no-repeat bg-center bg-cover mt-8 p-1 border-[1px] border-white/20 rounded-[10px] md:px-4">
+        <h1 className="text-[43px] sm:text-5xl md:text-6xl mb-9 mt-16 mx-auto text-center">
+          The Best Notes Site.
+        </h1>
+        <p className=" text-slate-100/70 text-lg md:text-xl mb-9 justify-center text-center ">
           Unlock the power of organization and productivity with our
           cutting-edge note-taking platform. Designed with simplicity and
           efficiency in mind, our site offers the most seamless experience for
           jotting down your thoughts, ideas, and to-do lists.
         </p>
         <div className="flex  justify-center">
+          <img
+            src={paperplane}
+            className="absolute w-0 md:w-40 sm:mt-80 md:ml-[450px] md:mt-6"
+          ></img>
           <Link to="/notes">
             <button
               className="px-9 py-5 bg-slate-900/70 mt-6 md:mt-12 rounded-lg border-2 border-slate-300 text-xl
